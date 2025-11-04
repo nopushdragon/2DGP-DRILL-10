7b853aada9e807e0b6ce5a267813f53ec76b42ea
AN MIN YONG <iks05280204@gmail.com>
2025-11-04 16:56:38 +0900

    일단 랜덤위치에 띄워서 애니메이션이 잘 작동하는지 (속도는 boy와 동일)

--- bird.py
+++ bird.py
@@ -15,10 +15,24 @@ FRAMES_PER_ACTION = 14
 class BIRD:
     image = None
     def __init__(self):
-        pass
+        self.x = random.randint(200, 1400)
+        self.y = random.randint(300, 500)
+        self.frame = 0
+        self.face_dir = 1 #1오-1왼
+        self.dir = 1  #1오-1왼
+
+        if BIRD.image == None:
+            BIRD.image = load_image('bird_animation.png')
 
     def draw(self):
-        pass
+        if int(self.frame) < 5:
+            self.image.clip_draw(int(self.frame) * 181, 338, 185, 168, self.x, self.y)
+        elif int(self.frame) < 10:
+            self.image.clip_draw((int(self.frame) - 5) * 181 , 169, 185, 168, self.x, self.y)
+        else:
+            self.image.clip_draw((int(self.frame) - 10) * 181 , 0, 185, 168, self.x, self.y)
 
     def update(self):
+        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 14
+
         pass
